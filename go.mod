e92218ff998e7c4896ee2e39d506f90ea2951aa2
Mislav Zanic <mislavzanic3@gmail.com>
2022-10-20 11:28:29 +0200

    [go] hello world type example

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module codeberg.org/mislavzanic/main
 
 go 1.18
+
+require (
+	github.com/russross/blackfriday v1.6.0 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/russross/blackfriday/v2"
 )
 
+func hello(w http.ResponseWriter, req *http.Request) {
+	body, _ := ioutil.ReadFile("./README.md")
+	output := blackfriday.Run(body)
+	fmt.Fprintf(w, "%v", template.HTML(output))
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
+	}
+}
+
 func main() {
-	fmt.Println("Hello")
+	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/headers", headers)
+
+	http.ListenAndServe(":8093", nil)
 }
