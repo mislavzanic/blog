b5bf1761b30859b86dff14008983c9f34b9cca75
Mislav Zanic <mislavzanic3@gmail.com>
2022-11-02 11:11:13 +0100

    [webhook] created a basic webhook for post updates

--- internal/webhook/webhook.go
+++ internal/webhook/webhook.go
@@ -1,13 +1,31 @@
 package webhook
 
 import (
-	"io/ioutil"
-	"log"
 	"net/http"
+	"os"
+	"log"
+
+	"github.com/go-git/go-git/v5"
+	"codeberg.org/mislavzanic/main/internal/posts"
 )
 
 
 func Webhook(w http.ResponseWriter, req *http.Request) {
-	body, _ := ioutil.ReadAll(req.Body)
-	log.Println(string(body))
+	if _, err := os.Stat(posts.GITDIR); !os.IsNotExist(err) {
+		r, err := git.PlainOpen(posts.GITDIR)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w, err := r.Worktree()
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := w.Pull(&git.PullOptions{RemoteName: "origin"}); err != nil {
+			log.Fatal(err)
+		}
+	}
 }
