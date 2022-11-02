3aa8dbf1fdb6a895ad85cbab0fd47820735e7840
Mislav Zanic <mislavzanic3@gmail.com>
2022-11-02 12:03:57 +0100

    Deleted images dir

--- internal/server/router.go
+++ internal/server/router.go
@@ -13,6 +13,7 @@ func NewRouter() *mux.Router {
 	cssDir := posts.CSSDIR
 	router := mux.NewRouter()
 	router.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir(cssDir))))
+	router.PathPrefix("/BlogPosts/").Handler(http.StripPrefix("/BlogPosts/", http.FileServer(http.Dir(posts.GITDIR))))
 	router.HandleFunc("/", posts.ViewAllPosts)
 	router.HandleFunc("/blog/{pageId}", posts.PageHandler)
 	router.HandleFunc("/by-tag/{tagId}", posts.FilterByTag)
