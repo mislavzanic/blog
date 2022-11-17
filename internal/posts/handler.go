2a841d47fab6611ceb92deaed98332d6663475af
Mislav Zanic <mislavzanic3@gmail.com>
2022-11-17 23:35:16 +0100

    [go] bugfix

--- internal/posts/handler.go
+++ internal/posts/handler.go
@@ -23,7 +23,7 @@ func PageHandler(w http.ResponseWriter, req *http.Request) {
 	pageId := mux.Vars(req)["pageId"]
 	p := readBlogPost(fmt.Sprintf("%s/%s.md", POSTSDIR, pageId))
 
-	renderFromTemplate(w, "post.html", fmt.Sprintf("%s/post.html", HTMLDIR), template.FuncMap{"markDown": markDowner}, p)
+	renderFromTemplate(w, "post.html", fmt.Sprintf("%s/post.html", HTMLDIR), template.FuncMap{"markDown": markDowner, "afterEpoch": AfterEpoch}, p)
 }
 
 func AboutSection(w http.ResponseWriter, req *http.Request) {
@@ -36,11 +36,11 @@ func FilterByTag(w http.ResponseWriter, req *http.Request) {
 	tagId := mux.Vars(req)["tagId"]
 	posts := findBlogPosts(tagId)
 
-	renderFromTemplate(w, "tags.html", fmt.Sprintf("%s/index.html", HTMLDIR), template.FuncMap{"toURL": getUrl, "markDown": markDowner}, posts)
+	renderFromTemplate(w, "tags.html", fmt.Sprintf("%s/index.html", HTMLDIR), template.FuncMap{"toURL": getUrl, "markDown": markDowner, "afterEpoch": AfterEpoch}, posts)
 }
 
 func ViewAllPosts(w http.ResponseWriter, req *http.Request) {
 	posts := getAllPosts()
-	renderFromTemplate(w, "index.html", fmt.Sprintf("%s/index.html", HTMLDIR), template.FuncMap{"toURL": getUrl, "markDown": markDowner}, posts)
+	renderFromTemplate(w, "index.html", fmt.Sprintf("%s/index.html", HTMLDIR), template.FuncMap{"toURL": getUrl, "markDown": markDowner, "afterEpoch": AfterEpoch}, posts)
 }
 
