7dfdb31615a9d51c61235be7e45f57407088fa9d
Mislav Zanic <mislavzanic3@gmail.com>
2022-11-27 00:31:53 +0100

    [ci] changed to flakes

---
+++ flake.nix
@@ -0,0 +1,94 @@
+{
+  description = "A simple Go package";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    let
+
+      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
+      version = builtins.substring 0 8 lastModifiedDate;
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          src = ./.;
+        in rec {
+          bin = pkgs.buildGoModule {
+            pname = "blog-bin";
+            inherit version;
+            src = ./.;
+            vendorSha256 = "sha256-gbABy8TTAF0EgFrNcIEXlVJxmlMGRXD28X48wMQfg6I=";
+          };
+
+          posts = pkgs.stdenv.mkDerivation {
+            pname = "blog-posts";
+            inherit (bin) version;
+            inherit src;
+
+            phases = "installPhase";
+
+            installPhase = ''
+              mkdir -p $out
+              cp -vrf $src/blog $out
+            '';
+          };
+
+          static = pkgs.stdenv.mkDerivation {
+            pname = "blog-static";
+            inherit (bin) version;
+            inherit src;
+
+            phases = "installPhase";
+
+            installPhase = ''
+              mkdir -p $out
+              cp -vrf $src/js $out
+              cp -vrf $src/html $out
+              cp -vrf $src/css $out
+            '';
+          };
+
+          default = pkgs.symlinkJoin {
+            name = "blog";
+            paths = [ posts static bin ];
+          };
+
+          docker = pkgs.dockerTools.buildLayeredImage {
+            name = "mislavzanic/blog";
+            tag = "dev";
+
+            contents = [ default ];
+
+            config = {
+              Cmd = [ "${bin}/bin/blog" ];
+              WorkingDir = "${default}";
+            };
+          };
+
+        });
+
+
+      apps = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+          upload-script = pkgs.writeShellScriptBin "upload-image" ''
+            set -eu
+            nix build .#docker
+            docker load < result
+            docker push $DOCKER_HUB_USERNAME/blog:dev
+          '';
+        in {
+          upload-script = flake-utils.lib.mkApp { drv = upload-script; };
+        }
+      );
+
+      defaultPackage = forAllSystems (system: self.packages.${system}.gotsm);
+    };
+}
